35b5f9b5ee5b0ca5ba3971bf99c81f91b329a063
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 06:00:39 -0700

    Create Q19.ts

---
+++ Day_07/Q19.ts
@@ -0,0 +1,5 @@
+
+// Dinner guests: print out a message on the number of people you invite to dinner and work with one of the programs for exercises 14 to 18
+
+let Dinner_invitation: string[] = ["Wardha", "Aiman", "Zunaiba", "Zimal", "Janifer", "Muqadas", "Zunaira", "Izma", "Haider"];      
+console.log("Hi!", Dinner_invitation.length, "people we're inviting to dinner");     
