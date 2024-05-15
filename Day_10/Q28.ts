4cba32f3aac30458252752aa23511777d8aa3bb7
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 08:49:00 -0700

    Create Q28.ts

---
+++ Day_10/Q28.ts
@@ -0,0 +1,29 @@
+//  Stages of Life: create an if-else chain to determine a person's stage of life. Specify a value for the variability age, and then
+
+let age : number = 45;
+
+
+if(age < 2){
+	console.log(" The person's a baby");
+}
+
+else if(age < 4){
+	console.log("The person's a little boy");
+}
+
+else if(age < 13){
+	console.log("The person's a kid");
+
+}
+
+else if(age < 20){
+	console.log("The person's a teenage boy");
+}
+
+else if(age < 65){
+	console.log("The person is an adult");
+}
+
+else{
+	console.log("The person's an old man");
+}
