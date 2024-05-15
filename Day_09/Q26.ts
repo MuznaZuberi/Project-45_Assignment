fb5f43f7c46fda7d6f54603e6f3eec7a7dd4286c
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 18:10:58 +0500

    Add files via upload

---
+++ Day_09/Q26.ts
@@ -0,0 +1,51 @@
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+
+let alien_color: string = "Green";
+
+
+console.log("\n");
+
+//1. If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+//To run the if block 
+
+alien_color = "Green";
+
+if(alien_color == "Green"){
+	console.log('"For shooting the alien, Congrats! you just got five points."');
+}
+
+
+
+
+
+//2.  If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+//To run the else block
+
+alien_color = "Red";
+
+if(alien_color == "Green"){
+	console.log('"For shooting the alien, the player just got five points."');
+}
+
+else{
+	console.log('"Congrats! You just earned 10 points"');
+}
+
+
+
+
+
+//3. To run the if block and the other block, write one version of the program
+
+//To run the else block
+
+alien_color == "Yellow";
+if(alien_color == "Green"){
+	console.log('"For shooting the alien, the player just got five points."');
+
+}
+
+else{
+	console.log('"Congrats! You just earned 10 points"');
+}
---
+++ Day_09/Q27.ts
@@ -0,0 +1,65 @@
+//  Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain
+
+ 
+// 1.   If the alien's green, print a message that the player has earned 5 points
+
+let alien_color: string = "Green";
+if(alien_color == "Yellow"){
+	console.log('"For shooting the alien, Congrats! you just got 10 points."');
+} 
+
+else if(alien_color == "Green"){
+	console.log('"For shooting the alien, Congrats! you just got 5 points."');
+}
+
+else if(alien_color == "Red"){
+	console.log('"For shooting the alien, Congrats! you just got 15 points."');
+}
+
+
+
+
+
+
+
+
+
+// 3.   If the alien's yellow, print a message that the player has earned 10 points
+let Alien_Color: string = "Yellow";
+
+if(Alien_Color == "Yellow"){
+	console.log('"For shooting the alien, Congrats! you just got 10 points."');
+} 
+
+else if(Alien_Color == "Green"){
+	console.log('"For shooting the alien, Congrats! you just got 5 points."');
+}
+
+else if(Alien_Color == "Red"){
+	console.log('"For shooting the alien, Congrats! you just got 15 points."');
+}
+
+
+
+
+
+
+
+//3.  If the alien's red, print a message that the player has earned 15 points
+
+
+let Alien_color: string = "Red";
+
+if(Alien_color == "Yellow"){
+	console.log('"For shooting the alien, Congrats! you just got 10 points."');
+} 
+
+else if(Alien_color == "Green"){
+	console.log('"For shooting the alien, Congrats! you just got 5 points."');
+}
+
+else if(Alien_color == "Red"){
+	console.log('"For shooting the alien, Congrats! you just got 15 points."');
+}
+
+
