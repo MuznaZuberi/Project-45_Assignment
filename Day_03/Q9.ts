6f5dadcce00870888026aef5cf2a6c6c7a74bad8
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 17:46:00 +0500

    Add files via upload

---
+++ Day_03/Q9.ts
@@ -0,0 +1,5 @@
+// Your favorite number: store your favorite number in a variable. Then create a message that displays your favourite number with this variable. Print that message.
+
+let favourite_number : number = 9;
+let messager = favourite_number;
+console.log("My_Favrt Numbe Is: ", messager);
