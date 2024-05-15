011eb9283bfdb6b1165e1c7c03649b591bde6bfb
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 05:45:15 -0700

    Update Q6.ts to Q_07_08.ts

---
+++ Day_03/Q_07_08.ts
@@ -0,0 +1,24 @@
+ //Number 8: Write an addition, subtraction, multiplication, and division operation that will result in the number eight. To see the results, be sure to write your operation in a print statement. Four lines that look like this, you should create: The output should be four lines, with the number 8 appearing on each line once.
+
+ //ADDITION 
+
+let addition: number = 1 + 7;
+console.log("Addition : ", addition);
+
+
+//SUBTRACTION
+
+let subtraction : number = 10 - 2;
+console.log("Subtraction : " , subtraction);
+
+
+//MULTIPLICATION
+
+let multiplication : number = 2 * 4;
+console.log("Multiplication : " , multiplication);
+
+
+//Division
+
+let division: number = 16 / 2;
+console.log("Division : " , division);
