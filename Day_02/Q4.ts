5d85b047bec233b2e5538b551ab7ef7bb58e779f
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 05:37:55 -0700

    Update Q4.ts

--- Day_02/Q4.ts
+++ Day_02/Q4.ts
@@ -1,1 +1,4 @@
+//A famous quote: Find a famous person you admire. Please print the quote and it's author's name. In addition to the quotation marks, your output should be as follows. "Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
+let famous_quote: string = "Napoleon Hill";
+console.log(famous_quote, 'once said,  "Patience, persistence, and perspiration make an unbeatable combination for success."');
