89b0179d22a7cf8f60f28a1f5b4a7a27fcbcf482
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 08:52:21 -0700

    Create Q31.ts

---
+++ Day_11/Q31.ts
@@ -0,0 +1,14 @@
+//  No users: To make sure that the user list is not blank, you need to include an if test in exercise 28.
+
+let users : string[] = [];
+
+// 1.  If you don't see any entries, run the message we need to find some users! 
+// 2.  Make sure that all the usernames are deleted from your list, and print a message properly.
+
+if(users == users){
+	console.log("We need to find some users!");
+}
+
+else{
+	// none
+}
