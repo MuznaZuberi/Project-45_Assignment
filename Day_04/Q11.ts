e95d6a7113d559568be6b986a28fa701157cdccd
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 17:48:52 +0500

    Add files via upload

---
+++ Day_04/Q11.ts
@@ -0,0 +1,8 @@
+// Names: In a row called names, store the names of some of your friends. Access each element in the list, one at a time, to print the names of all persons.
+
+
+
+let My_few_friends: string[] = ["Aizal", "Zimal", "Zaviyar", "Zaid", "Haider", "Sundus", "Zunaira", "Minal"];
+for(let friends of  My_few_friends){
+	console.log(friends);
+}
---
+++ Day_04/Q12.ts
@@ -0,0 +1,6 @@
+// Welcome: start by using the array you used in Exercise 11, but instead of printing everyone's name, print a message to them. Each message should contain the same text, but each one should be personalized with a particular person's name.
+
+let Persons_names : string[] = ["Aizal", "Zimal", "Zaviyar", "Zaid", "Haider", "Sundus", "Zunaira", "Minal"];
+for(let person of Persons_names){
+	console.log("Hi!", person, "How are u?", "Hope u are doing well!");
+}
