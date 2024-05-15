82dd73f3789050749dc35a83dc2f30bba20133cf
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 06:10:13 -0700

    Create Q25.ts

---
+++ Day_09/Q25.ts
@@ -0,0 +1,20 @@
+//  Aliens Colors 1 : Imagine that in a game, an alien was shot down. Create a variable called alien_color and assign a value of "green", "yellow", or "red" to it.
+
+let alien_color: string = "Green";
+
+// 1.  Write an if statement to test whether the alien’s color is green. If that's the case, print out a report indicating that this player has gotten five points.
+
+if(alien_color == "Green"){
+	console.log('"Five points have just been awarded to the player"');
+}
+
+
+
+
+//2.  • Make a version of this program that passes the if test and one that fails. No output will be given if the version is not successful.
+
+alien_color = "Yellow";
+
+if(alien_color == "Green"){
+  //"No output will be given if the version is not successful";
+}
