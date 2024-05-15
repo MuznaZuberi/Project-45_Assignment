d6bb93fd9e7420d403f0540803d2c50d1cb320a5
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 06:04:58 -0700

    Create Q22.ts

---
+++ Day_08/Q22.ts
@@ -0,0 +1,11 @@
+ //Intentional error: If one of your programs has not yet encountered an array index error, try to fix it yourself. If you want to produce an index error, change the index on one of your programs. Before you close the program, check to see if your error has been corrected.
+
+ let colours : string[] = ["Red" , "Blue" , "Green" , "Yellow" , "Browm" , "Grey"];
+
+ console.log("This is an intentional error");
+ console.log(colours[7]); //Intentional error
+
+ console.log("\n");
+
+ console.log("To correct an error");
+ console.log(colours[4]); //Accessing a valid index to correct an error
