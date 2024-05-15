d23cbdcfc8c593171282afe68c3364d798206964
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 18:01:22 +0500

    Add files via upload

---
+++ Day_07/Q20.ts
@@ -0,0 +1,5 @@
+//Think of something you could store in an array. A list of mountains, rivers, countries, cities, languages, or anything else you want can be drawn up. Create a program that creates lists containing those items.
+let Mountains: string[] = ["Mount Everest", "Mount Kilimanjaro", "Mount Fuji", "Mount Rainier" , "Denali" , "Matterhorn"];
+console.log("\n");
+console.log(" I'd like to take a look at these mountains : ");
+console.log(Mountains);
---
+++ Day_07/Q21.ts
@@ -0,0 +1,19 @@
+//They're thinking about storing something in a TypeScript object. Create a program to create objects that contain these elements.
+
+let Iphone : {
+    Maker : "string",
+    CEO : "string",
+    Launch : "number",
+},
+
+iPhone = {
+	Maker : "Apple",
+	CEO : "Steve Jobs",
+	Launch : 2007,
+};
+
+console.log(` Iphone History : ` , `Iphone_Maker : ${iPhone.Maker} ` ,` Iphone_CEO : ${iPhone.CEO} ` , ` Iphone_launch : ${iPhone.Launch} `); 
+
+
+
+
