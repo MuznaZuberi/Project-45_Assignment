995e4cce1e41c5f79c73d90f5e26f0320f2be1c8
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-15 05:48:07 -0700

    Create Q10.ts

---
+++ Day_04/Q10.ts
@@ -0,0 +1,13 @@
+ //Adding comments: Select two programs you have written, and add at least one comment to each. You can add your name and the most recent date at the top of each program file if you don't have anything special for writing because programs are so easy to use right now. Then write down one sentence describing what this program does.
+
+
+
+
+/*let person_name: string = "Janifer";
+console.log("Hello!" , person_name , "would you like to learn some Python today ?");*/
+
+
+
+
+/*let Persons_name1: string = Persons_name.trim();
+console.log("Striping: ", Persons_name1);*/
