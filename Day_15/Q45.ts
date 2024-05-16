2631c11a3ea6e69b8272d8933899d08dbd14c714
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-16 03:58:08 -0700

    Update Q45.ts

--- Day_15/Q45.ts
+++ Day_15/Q45.ts
@@ -1,6 +1,6 @@
 //  Cars: Create a function to store information about the vehicle in an object. A manufacturer and model name should always be provided for the function. An arbitrary number of keyword arguments should therefore be accepted. The function needs to be called with the information requested, as well as two additional pairs of name=value: color or optional features. To ensure that all information is correctly stored, print the object that has been returned.
 
-function Car(){
+function Car(...other :string[]){
 	let obj = {
 		manufacturer: "Honda",
 		modern: "Civic",
@@ -9,9 +9,7 @@ function Car(){
 			features: "Sunroof",
 		}
 	};
-
 	return obj;
 }
-let res = Car();
 
-console.log(res);
+console.log(Car());
